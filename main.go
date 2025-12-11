d86409a3034cb23db92bc6b7748735799b8215fd
Geass <525647740@qq.com>
2025-12-11 23:42:01 +0800

    update fetch

--- main.go
+++ main.go
@@ -16,36 +16,38 @@ import (
 
 const BASE_URL = "http://www.mzitu.cc"
 
-type Ablum struct{
-	Title string `json:"title"`
-	Href string `json:"href"`
-	Id int `json:"id"`
-	Cover string `json:"cover"`
+type Ablum struct {
+	Title  string   `json:"title"`
+	Href   string   `json:"href"`
+	Id     int      `json:"id"`
+	Cover  string   `json:"cover"`
 	Images []string `json:"images"`
 }
 
-func (ablum *Ablum)FillAndDownloadImages(group *sync.WaitGroup) []string {
+func (ablum *Ablum) FillAndDownloadImages(group *sync.WaitGroup) []string {
 	defer group.Done()
 
 	var url string = ablum.Href
 	fmt.Println("download cover image:", ablum.Cover)
-	coverLocalFile := fmt.Sprintf("imgs/%d_%d_cover.jpg",ablum.Id, time.Now().UnixMilli())
+	coverLocalFile := fmt.Sprintf("imgs/%d_%d_cover.jpg", ablum.Id, time.Now().UnixMilli())
 	err := DownloadFile(coverLocalFile, ablum.Cover)
 	var noCover bool = false
-	if(err != nil){
+	if err != nil {
 		fmt.Println("download cover image error")
 		os.Remove(coverLocalFile)
 		noCover = true
 	}
 
+	ablum.Cover = coverLocalFile
+
 	imageList := []string{}
-	visitedUrl := make(map[string] bool)
+	visitedUrl := make(map[string]bool)
 	currentUrl := url
 	visitedUrl[currentUrl] = true
 	for {
 		fmt.Println("request url", currentUrl)
 		resp, err := http.Get(currentUrl)
-		if err != nil || resp.StatusCode != 200{
+		if err != nil || resp.StatusCode != 200 {
 			resp.Body.Close()
 			fmt.Println("request error quit.")
 			return imageList
@@ -59,36 +61,39 @@ func (ablum *Ablum)FillAndDownloadImages(group *sync.WaitGroup) []string {
 
 		aTag := doc.Find(".main-image a")
 		// fmt.Println(aTag.Html())
-		href,exist := aTag.Attr("href")
-		if(!exist){
+		href, exist := aTag.Attr("href")
+		if !exist {
 			resp.Body.Close()
 			break
 		}
 		fmt.Println(href)
 		imageUrl, srcExist := aTag.Find("img").Attr("src")
-		if(srcExist && imageUrl != ""){
-			imageList = append(imageList, imageUrl)
+		if srcExist && imageUrl != "" {
 			curTime := time.Now()
-			DownloadFile(fmt.Sprintf("imgs/%d_%d.jpg", ablum.Id, curTime.UnixMilli()), imageUrl)
+			imageLocalFile := fmt.Sprintf("imgs/%d_%d.jpg", ablum.Id, curTime.UnixMilli())
+			err = DownloadFile(imageLocalFile, imageUrl)
+			if err == nil {
+				imageList = append(imageList, imageLocalFile)
+			}
 		}
-		
-		currentUrl = BASE_URL+ href
-		if(visitedUrl[currentUrl]){
+
+		currentUrl = BASE_URL + href
+		if visitedUrl[currentUrl] {
 			break
 		}
 
 		visitedUrl[currentUrl] = true
 		resp.Body.Close()
-	}//end for each
-	
+	} //end for each
+
 	ablum.Images = imageList
-	if(noCover && len(ablum.Images) > 0){
+	if noCover && len(ablum.Images) > 0 {
 		ablum.Cover = ablum.Images[0]
 	}
 	return imageList
 }
 
-func fetchAblums(url string) []Ablum{
+func fetchAblums(url string) []Ablum {
 	fmt.Println("fetchAblums")
 
 	var ablumList []Ablum = []Ablum{}
@@ -99,7 +104,7 @@ func fetchAblums(url string) []Ablum{
 	}
 
 	defer resp.Body.Close()
-	
+
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
 		return ablumList
@@ -107,22 +112,22 @@ func fetchAblums(url string) []Ablum{
 	doc.Find(".postlist li").Each(func(i int, s *goquery.Selection) {
 		aTag := s.Find("a").First()
 		href, _ := aTag.Attr("href")
-		
-		fmt.Println("href",href)
-		
+
+		fmt.Println("href", href)
+
 		imgTag := s.Find("a img")
-		cover,_ := imgTag.Attr("data-original")
-		title,_ := imgTag.Attr("alt")
-		fmt.Println("cover",cover)
-		fmt.Println("title",title)
+		cover, _ := imgTag.Attr("data-original")
+		title, _ := imgTag.Attr("alt")
+		fmt.Println("cover", cover)
+		fmt.Println("title", title)
 		id := GetAidFromHref(href)
-		fmt.Println("id",id)
+		fmt.Println("id", id)
 
 		var ablum Ablum = Ablum{
 			Title: title,
-			Href : BASE_URL + href,
-			Cover : cover,
-			Id : id,
+			Href:  BASE_URL + href,
+			Cover: cover,
+			Id:    id,
 		}
 
 		ablumList = append(ablumList, ablum)
@@ -132,15 +137,15 @@ func fetchAblums(url string) []Ablum{
 }
 
 func GetAidFromHref(href string) int {
-	if(href == ""){
+	if href == "" {
 		return -1
 	}
 
-	var preIdx int = strings.LastIndex(href,"/")
-	var endIdx int = strings.LastIndex(href,".html")
-	var subStr = href[preIdx + 1:endIdx]
+	var preIdx int = strings.LastIndex(href, "/")
+	var endIdx int = strings.LastIndex(href, ".html")
+	var subStr = href[preIdx+1 : endIdx]
 	value, err := strconv.Atoi(subStr)
-	if(err != nil){
+	if err != nil {
 		return -1
 	}
 	return value
@@ -152,7 +157,7 @@ func DownloadFile(filepath string, url string) error {
 		return err
 	}
 	defer out.Close()
-	
+
 	// 发起 HTTP 请求
 	resp, err := http.Get(url)
 	if err != nil {
@@ -170,25 +175,42 @@ func DownloadFile(filepath string, url string) error {
 	return err
 }
 
-
-func PrepareDirs(){
+func PrepareDirs() {
 	fmt.Println("Prepare dirs.")
-	os.Mkdir("data",0777)
-	os.Mkdir("imgs",0777)
+	os.Mkdir("data", 0777)
+	os.Mkdir("imgs", 0777)
 }
 
-func main() {
-	PrepareDirs()
-
-	ablums := fetchAblums("http://www.mzitu.cc/page/index_10.html")
+func FetchPage(pageUrl string) []Ablum {
+	ablums := fetchAblums(pageUrl)
 	var wg sync.WaitGroup
-	
+
 	for i := range ablums {
 		wg.Add(1)
 		go ablums[i].FillAndDownloadImages(&wg)
 	}
 	wg.Wait()
+	return ablums
+}
 
-	jsonData , _ := json.Marshal(ablums)
-	os.WriteFile("data/1.json",jsonData,0777)
+func main() {
+	PrepareDirs()
+	var ablumsList = make([]Ablum, 0, 2048)
+	const TotalSize = 38
+	for i := TotalSize; i >= 1; i-- {
+		var url string
+		if i == 1 {
+			url = "http://www.mzitu.cc"
+		} else {
+			url = fmt.Sprintf("http://www.mzitu.cc/xinggan/index_%d.html", i)
+		}
+
+		fmt.Println("Progress : ", i-TotalSize, "/", TotalSize)
+		fmt.Println("Catch page : ", url)
+		sublist := FetchPage(url)
+		ablumsList = append(ablumsList, sublist...)
+	}
+
+	jsonData, _ := json.Marshal(ablumsList)
+	os.WriteFile("data/all.json", jsonData, 0777)
 }
